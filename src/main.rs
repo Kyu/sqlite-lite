13130da6e19c207de7fa4b896c15c9ca93ed13c7
Precious Oyetunji <Kyu@users.noreply.github.com>
2023-12-13 22:04:02 -0600

    Added file persistence, added fixed length strings, added max length tests [tests fail due to unrelated issue]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::io;
-use std::io::Write;
+use std::{env, io};
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::os::unix::fs::FileExt;
+use std::path::Path;
 use std::process::exit;
+
 use crate::ExecuteResult::{ExecuteFailed, ExecuteSuccess, ExecuteTableFull};
-
 use crate::MetaCommandResult::{MetaCommandSuccess, MetaCommandUnrecognized};
-use crate::PreparedStatementResult::{PreparedStatementSuccess, PreparedStatementSyntaxError, PreparedStatementUnrecognized};
+use crate::PreparedStatementResult::{PreparedStatementError, PreparedStatementSuccess, PreparedStatementSyntaxError, PreparedStatementUnrecognized};
 use crate::StatementType::Invalid;
-
 
 // https://stackoverflow.com/a/70222282/3875151
 macro_rules! size_of_attribute {
@@ -27,9 +29,9 @@ macro_rules! size_of_attribute {
     }};
 }
 
-static ID_SIZE: usize = size_of_attribute!(SimpleRow::id);
-static USERNAME_SIZE: usize = size_of_attribute!(SimpleRow::username);
-static EMAIL_SIZE: usize = size_of_attribute!(SimpleRow::email);
+const  ID_SIZE: usize = 4;
+const  USERNAME_SIZE: usize = 32;
+const  EMAIL_SIZE: usize = 255;
 
 static ID_OFFSET: usize = 0;
 static USERNAME_OFFSET: usize = ID_OFFSET + ID_SIZE;
@@ -43,11 +45,34 @@ static ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
 static TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
-fn main() {
-    let mut table: Table = Table::new();
-    let mut input_buffer: InputBuffer = InputBuffer::new();
+fn main() -> io::Result<()>{
+    let args: Vec<String> = env::args().collect();
 
-    loop {
+    if args.len() <= 0 {
+        let exc_path: String;
+        if args.len() != 0 {
+            exc_path = args[0].to_string();
+        } else {
+            exc_path = "sqlite_lite".to_string();
+        }
+        println!("{}", usage(exc_path));
+        println!("Goodbye!");
+        exit(1);
+    }
+
+    let db_file_name = args[1].to_string();
+    let mut table: Table = Table::new(&db_file_name);
+
+    if ! table.file_ok {
+        println!("File {file} doesn't exist and could not be created. Goodbye!", file=db_file_name);
+        exit(1);
+    }
+
+    let mut program_state: ProgramState = ProgramState::new();
+    let mut input_buffer: InputBuffer = InputBuffer::new();
+    program_state.running = true;
+
+    while program_state.running {
         print_prompt();
         read_input(&mut input_buffer);
 
@@ -55,7 +80,7 @@ fn main() {
         let mut statement: PreparedStatement = PreparedStatement::new();
 
         if input.starts_with(".") {
-            match do_meta_command(input) {
+            match do_meta_command(input, &mut program_state) {
                 MetaCommandSuccess => {
                     continue;
                 }
@@ -75,6 +100,10 @@ fn main() {
                     println!("Syntax error in: {:?}", input);
                     continue;
                 }
+                PreparedStatementError => {
+                    println!("Could not prepare statement: {:?}", input);
+                    continue;
+                }
             }
         }
 
@@ -87,6 +116,10 @@ fn main() {
             ExecuteTableFull => {println!("Table is full!")}
         }
     }
+
+    table.drop();
+    println!("Goodbye!");
+    Ok(())
 }
 
 fn read_input(input_buffer: &mut InputBuffer) {
@@ -96,11 +129,10 @@ fn read_input(input_buffer: &mut InputBuffer) {
         .expect("Could not parse input!");
 }
 
-fn do_meta_command(command: &str) -> MetaCommandResult{
+fn do_meta_command(command: &str, program_state: &mut ProgramState) -> MetaCommandResult{
     match command {
         ".exit" => {
-            println!("Goodbye!");
-            exit(0);
+            program_state.running = false;
         },
         ".test" => println!("Test worked!"),
         _ => return MetaCommandUnrecognized
@@ -125,8 +157,12 @@ fn do_prepared_statements(input: &str, statement: &mut PreparedStatement) -> Pre
             return PreparedStatementSyntaxError
         }
 
-        statement.row.username = split_info.get(2).unwrap().parse().unwrap();
-        statement.row.email = split_info.get(3).unwrap().parse().unwrap();
+        if ! statement.row.set_username(split_info.get(2).unwrap().parse().unwrap()) {
+            return PreparedStatementError
+        }
+        if ! statement.row.set_email(split_info.get(3).unwrap().parse().unwrap()) {
+            return PreparedStatementError
+        }
     } else {
         return PreparedStatementUnrecognized
     }
@@ -149,7 +185,7 @@ fn execute_prepared_statement(statement: PreparedStatement, table: &mut Table) -
     exec_result
 }
 
-fn set_row_slot(table: &mut Table, row: SimpleRow, _row_num: usize) -> bool {
+fn set_row_slot(table: &mut Table, row: SimpleRow, _row_num: usize) -> bool { // TODO make member function
     // let page_num: usize = row_num / ROWS_PER_PAGE;
     // let row_slot = row_num % ROWS_PER_PAGE;
 
@@ -171,7 +207,7 @@ fn set_row_slot(table: &mut Table, row: SimpleRow, _row_num: usize) -> bool {
     true
 }
 
-fn append_row_slot(table: &mut Table, row: SimpleRow) -> bool {
+fn append_row_slot(table: &mut Table, row: SimpleRow) -> bool { // TODO make member function
     return set_row_slot(table, row, &table.num_rows + 1);
 }
 
@@ -196,8 +232,8 @@ fn execute_select(_statement: PreparedStatement, table: &mut Table) -> ExecuteRe
     ExecuteSuccess
 }
 
-fn print_row(row: &SimpleRow) {
-    println!("({id}, {username}, {email})", id=row.id, username=row.username, email=row.email)
+fn print_row(row: &SimpleRow) { // TODO make member function
+    println!("({id}, {username}, {email})", id=row.id, username=row.get_username(), email=row.get_email())
 }
 
 fn print_prompt() {
@@ -205,16 +241,140 @@ fn print_prompt() {
     io::stdout().flush().unwrap();
 }
 
+fn usage(exec_path: String) -> String {
+    format!("Usage: {exec_path} <database_file_name>")
+}
+
+/*
+struct Varchar {
+    size: u8,
+    value: Vec<char>
+}
+
+impl Varchar {
+    pub fn new(size: u8) -> Self {
+        Varchar {
+            size,
+            value: Vec::with_capacity(size as usize)
+        }
+    }
+
+    fn set_value(&mut self, new_value: String) -> bool { // TODO add getters/setters https://stackoverflow.com/a/44879870/3875151
+        if new_value.len() > self.size as usize {
+            return false;
+        }
+
+        let mut index = 0;
+        for chr in new_value.chars() {
+            if self.value.len() <= index {
+                self.value.push(chr);
+            } else {
+                self.value[index] = chr;
+            }
+
+            index += 1;
+        }
+
+        return true;
+    }
+
+    fn to_string(&self) -> String {
+        self.value.iter().collect()
+    }
+}
+*/
+
 struct SimpleRow {
     id: u32,
-    username: String,
-    email: String
+    username: [char; USERNAME_SIZE],
+    username_len: usize,
+    email: [char; EMAIL_SIZE],
+    email_len: usize
+}
+
+impl SimpleRow {
+    pub fn new() -> Self {
+        SimpleRow {
+            id: 0,
+            username: ['\0'; USERNAME_SIZE],
+            username_len: 0,
+            email: ['\0'; EMAIL_SIZE],
+            email_len: 0
+        }
+    }
+
+    fn get_username(&self) -> String {
+        return String::from_iter(&self.username[0..self.username_len]);
+    }
+
+    fn set_username(&mut self, new_username: String) -> bool {
+        if new_username.len() > self.username.len() {
+            return false;
+        }
+
+        let username_bytes = new_username.as_bytes();
+
+        for i in 0..self.username.len() {
+            if i < new_username.len() {
+                self.username[i] = username_bytes[i] as char;
+            } else {
+                self.username[i] = '\0';
+            }
+        }
+
+        self.username_len = new_username.len();
+        return true;
+    }
+
+    fn get_email(&self) -> String {
+        return String::from_iter(&self.email[0..self.email_len]);
+    }
+
+    fn set_email(&mut self, new_email: String) -> bool {
+        if new_email.len() > self.email.len() {
+            return false;
+        }
+
+        let email_bytes = new_email.as_bytes();
+
+        for i in 0..self.email.len() {
+            if i < new_email.len() {
+                self.email[i] = email_bytes[i] as char;
+            } else {
+                self.email[i] = '\0';
+            }
+        }
+
+        self.email_len = new_email.len();
+        return true;
+    }
+
+    fn get_id(&self) -> u32 {
+        return self.id;
+    }
+
+    fn set_id(&mut self, new_id: u32) -> bool {
+        self.id = new_id;
+        return true;
+    }
 }
 
 enum StatementType {
     Invalid,
     Insert,
     Select
+}
+
+struct ProgramState {
+    running: bool
+}
+
+impl ProgramState {
+    pub fn new() -> Self {
+        ProgramState {
+            running: false,
+        }
+    }
 }
 
 struct PreparedStatement {
@@ -224,11 +384,7 @@ struct PreparedStatement {
 
 impl PreparedStatement {
     pub fn new() -> Self {
-        let row = SimpleRow {
-            id: 0,
-            username: "".to_string(),
-            email: "".to_string(),
-        };
+        let row = SimpleRow::new();
 
         PreparedStatement {
             statement_type: Invalid,
@@ -237,17 +393,140 @@ impl PreparedStatement {
     }
 }
 
+
+
 struct Table {
     num_rows: usize,
     rows: Vec<SimpleRow>,
+    file_ok: bool,
+    file: File
 }
 
 impl Table {
-    pub fn new() -> Table {
-        Table {
+    pub fn new(db_file_name: &String) -> Table {
+        let file_existed = Path::new(db_file_name).exists();
+        let db_file = File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(db_file_name);
+
+
+        if ! file_existed {
+            let res = db_file.as_ref().unwrap().set_len((PAGE_SIZE * TABLE_MAX_PAGES * ROW_SIZE / ROWS_PER_PAGE) as u64);
+            if ! res.is_ok() {
+                println!("Tried to create file {db_file_name}, could not!");
+            }
+        }
+
+        if ! db_file.is_ok() {
+            return Table {
+                num_rows: 0,
+                rows: Vec::with_capacity(0),
+                file_ok: false,
+                file: db_file.unwrap(),
+            }
+        }
+
+        let mut new_table = Table {
             num_rows: 0,
             rows: Vec::with_capacity(TABLE_MAX_PAGES * PAGE_SIZE),
+            file_ok: true,
+            file: db_file.unwrap()
+        };
+        new_table.read_rows_from_file().expect("TODO: panic message");
+
+        return new_table;
+    }
+
+    fn write_at_position(&self, buf: &Vec<u8>, position: u64) -> bool{
+        let write = self.file.write_all_at(&buf, position);
+        if ! write.is_ok() {
+            println!("Problem writing to database file at position {}.", position);
+            return false;
         }
+
+        return true;
+    }
+
+    fn write_rows_to_file(&mut self) {
+        if self.rows.len() == 0 {
+            return;
+        }
+        // println!("Writing!");
+
+        if !self.file_ok {
+            println!("There was a problem when initially opening the database file, will not attempt to write to it!");
+            return;
+        }
+
+        let mut rows_written = 0;
+
+        for row in &self.rows {
+            // We now write at the offset 10.
+            // file.write_all_at(b"sushi", 10)?;
+            let mut row_buf: Vec<u8> = vec![];
+            row_buf.extend_from_slice(&row.id.to_le_bytes());
+            self.write_at_position(&row_buf, (rows_written * ROW_SIZE + ID_OFFSET) as u64);
+
+            row_buf = vec![];
+            write!(row_buf, "{}", &row.get_username()).expect("Could not write to Vec buffer. Out of memory?");
+            self.write_at_position(&row_buf, (rows_written * ROW_SIZE + USERNAME_OFFSET) as u64);
+
+            row_buf = vec![];
+            write!(row_buf, "{}", &row.get_email()).expect("Could not write to Vec buffer. Out of memory?");
+            self.write_at_position(&row_buf, (rows_written * ROW_SIZE + EMAIL_OFFSET) as u64);
+
+            rows_written += 1;
+        }
+    }
+
+    fn seek_read(&mut self, offset: u64, buf: &mut Vec<u8>) -> io::Result<()> {
+        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.read_exact(buf)?;
+        Ok(())
+    }
+
+    fn read_rows_from_file(&mut self) -> io::Result<()> {
+        // println!("Reading!");
+
+        let mut row_count = 0;
+        loop {
+            let mut v = vec![0; ID_SIZE];
+            self.seek_read((row_count * ROW_SIZE + ID_OFFSET) as u64, &mut v)?;
+            let id = u32::from_le_bytes(v.clone().try_into().expect("handle the error however you want"));
+            if id == 0 {
+                break;
+            }
+
+            v = vec![0; USERNAME_SIZE];
+            self.seek_read((row_count * ROW_SIZE + USERNAME_OFFSET) as u64, &mut v)?;
+            let username = String::from_utf8(v.clone()).unwrap_or("".to_string());
+
+            v = vec![0; EMAIL_SIZE];
+            self.seek_read((row_count * ROW_SIZE + EMAIL_OFFSET) as u64, &mut v)?;
+            let email = String::from_utf8(v.clone()).unwrap_or("".to_string());
+
+            let mut row = SimpleRow::new();
+
+            row.set_id(id);
+
+            row.set_username(username);
+            row.set_email(email);
+
+
+
+            // TODO move into one function
+            self.rows.push(row);
+            self.num_rows += 1;
+
+            row_count += 1;
+        }
+        Ok(())
+    }
+
+    fn drop(&mut self) {
+        self.write_rows_to_file();
     }
 }
 
@@ -271,7 +550,8 @@ enum MetaCommandResult {
 enum PreparedStatementResult {
     PreparedStatementSuccess,
     PreparedStatementSyntaxError,
-    PreparedStatementUnrecognized
+    PreparedStatementUnrecognized,
+    PreparedStatementError
 }
 
 enum ExecuteResult {
--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,14 @@
+import os
+import random
 import shutil
+import string
 
 import pexpect
 from ivoire import describe
 
 EXEC_PREFIX = "db > "
+SQLITE_LITE_EXEC_NAME = "sqlite_lite"
+DB_FILENAME = "db_file"
 
 
 class SqliteLite:
@@ -11,11 +16,16 @@ class SqliteLite:
 
 
 def get_executable():
-    shutil.copy('../target/debug/sqlite_lite', '.')
+    shutil.copy(f'../target/debug/{SQLITE_LITE_EXEC_NAME}', '.')
+
+
+def delete_old_db():
+    if os.path.isfile(DB_FILENAME):
+        os.remove(DB_FILENAME)
 
 
 def run_executable(inputs: list[str]):
-    c = pexpect.spawnu('./sqlite_lite')
+    c = pexpect.spawnu(f'./{SQLITE_LITE_EXEC_NAME} {DB_FILENAME}')
     final = ""
 
     for cmd in inputs:
@@ -28,28 +38,12 @@ def run_executable(inputs: list[str]):
 
     return resp
 
-    # c.expect('db > ')
-    # c.expect(pexpect.EOF)
-    # c.expect(r'.+')
-    # c.sendline(".test")
-    # print(c.after)
-    # print(c.before)
-    # c.expect('db > ')
-    # c.sendline(".test")
-
-    # c.expect('\n')  # \r\n
-    # print(c.before)
-    # c.sendline(".exit")
-    # print(c.before)
-
-    # c.interact()
-    # print(c.before)
-    # print(c.readlines())
-
 
 with describe(SqliteLite) as it:
     @it.before
     def before(_test):  # Takes at least 1 parameter, test, which can be used in other `it()`. Kinda like Object.self
+        _test.maxDiff = None
+        delete_old_db()
         get_executable()
 
     with it("inserts and retrieves a row") as test:
@@ -78,22 +72,45 @@ with describe(SqliteLite) as it:
         result = run_executable(input_list)
         test.assertCountEqual(expected, result)
 
-    with it("prints error message when table is full") as test:
-        input_list = []
+    # with it("prints error message when table is full") as test:
+    #     input_list = []
+    #
+    #     for i in range(7802):
+    #         input_list.append(f"insert {i} user{i} person{i}@example.com")
+    #
+    #     input_list.append(".exit")
+    #
+    #     result = run_executable(input_list)
+    #
+    #     test.assertIn("Table is full!", result)
 
-        for i in range(7802):
-            input_list.append(f"insert {i} user{i} person{i}@example.com")
+    with it("prints error message when string input is too long") as test:
+        long_username = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(33))
+        long_email = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(256))
 
-        input_list.append(".exit")
+        input_list = [
+            f"insert 1 {long_username} person1@example.com",
+            f"insert 2 person {long_email}",
+            ".exit",
+        ]
+
+        responses = [
+            f'Could not prepare statement: "{input_list[0]}"',
+            f'Could not prepare statement: "{input_list[1]}"',
+            "Goodbye!"
+        ]
+
+        expected = [
+            f"{EXEC_PREFIX}{input_list[0]}",
+            f"{responses[0]}",
+            f"{EXEC_PREFIX}{input_list[1]}",
+            f"{responses[1]}",
+            f"{EXEC_PREFIX}{input_list[2]}",
+            f"{responses[2]}"
+        ]
 
         result = run_executable(input_list)
-
-        print(result[-13:])
-        test.assertIn("Table is full!", result, )
-
-    # with it("prints error message when string input is too long") as test:
-    #     assert False
-    #     pass
+        test.assertCountEqual(expected, result)
 
     with it("prints an error message if id is negative") as test:
         input_list = [
@@ -120,6 +137,38 @@ with describe(SqliteLite) as it:
         result = run_executable(input_list)
         test.assertCountEqual(expected, result)
 
+    with it("keeps data after closing connection") as test:
+        input_list1 = [
+            "insert 1 user1 person1@example.com",
+            ".exit",
+        ]
+
+        expected1 = [
+            f"{EXEC_PREFIX}{input_list1[0]}",
+            "Execution Success!",
+            f"{EXEC_PREFIX}{input_list1[1]}",
+            "Goodbye!"
+        ]
+
+        result1 = run_executable(input_list1)
+        test.assertCountEqual(expected1, result1)
+
+        input_list2 = [
+            "select",
+            ".exit",
+        ]
+
+        expected2 = [
+            f"{EXEC_PREFIX}{input_list2[0]}",
+            "(1, user1, person1@example.com)",
+            "Execution Success!",
+            f"{EXEC_PREFIX}{input_list2[1]}",
+            "Goodbye!"
+        ]
+
+        result2 = run_executable(input_list2)
+        print(result2)
+        test.assertCountEqual(expected2, result2)
 
 if __name__ == "__main__":
     get_executable()
